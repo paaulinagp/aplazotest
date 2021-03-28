f20cefe215fa0ea030eb8b27163494e93fb58633
Paulina Guerrero <paaulinagp@gmail.com>
2021-03-27 23:41:30 -0600

    feat(dialog): se agregan validaciones de campos requeridos en el dialogo

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NoteInterface } from '../../interfaces/note.interface';
 @Component({
@@ -6,14 +7,37 @@ import { NoteInterface } from '../../interfaces/note.interface';
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
+
+  /**
+   * Formulario de creación de una nota
+   */
+   note: FormGroup;
 
   constructor(
     public dialogRef: MatDialogRef<any>,
-    @Inject(MAT_DIALOG_DATA) public data: NoteInterface) {}
+    @Inject(MAT_DIALOG_DATA) public data: NoteInterface,
+    private fb: FormBuilder) {}
 
-  onNoClick(): void {
-    this.dialogRef.close();
+  /**
+   * Ciclo de vida OnInit
+   *
+   * Se inicializa el formulario para crear una nota con validaciones sencillas
+   */
+  ngOnInit() {
+    this.note = this.fb.group({
+      id: new FormControl( this.data.id, [Validators.required]),
+      date: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      content: new FormControl('', [Validators.required]),
+      status: new FormControl('active', [Validators.required]),
+    });
   }
 
+  /**
+   * Función para cerrar dialogo
+   */
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
